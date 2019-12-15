abd26593bbed5afdbc673cf421ddd14f9c79b4b1
hjc3613 <37894838+hjc3613@users.noreply.github.com>
2019-12-15 21:36:57 +0800

    Update batch_normal_implement.py
    batch normal 计算过程理解

--- batch_normal_implement.py
+++ batch_normal_implement.py
@@ -1,7 +1,7 @@
 import torch
 input = torch.randn(20, 4, 50, 60) #B, C, H, W
-# batch normal， 对每一个channel，shape=(20, 50, 60)，计算each_channel.mean(), each_channel_std
-mean = input.mean(dim=1)
-std = input.std(dim=1)
-# batch normal计算
-output = (input-mean)/std
+# batch normal是在每一个channel层中计算均值，方差，然后利用均值、方差修正该层的输入值
+channel_1 = input[:, 0, :, :]
+mean_1 = channel_1.mean()
+std_1 = channel_1.std()
+batched_1 = (channel_1 - mean_1)/std_1
