0bcb645e01df2c95a71e2c3bfdce5ff39ea1a908
hjc3613 <37894838+hjc3613@users.noreply.github.com>
2020-06-06 15:53:55 +0800

    Update some_shell.sh

--- some_shell.sh
+++ some_shell.sh
@@ -1,3 +1,5 @@
 #windows 下压缩包，在linux下去解压，解决乱码问题
 LANG=C 7za x your-zip-file.zip
 convmv -f GBK -t utf8 --notest -r .
+# 批量修改文件名字
+find . -type f -name "* *.xml" -exec bash -c 'mv "$0" "${0// /_}"' {} \;
