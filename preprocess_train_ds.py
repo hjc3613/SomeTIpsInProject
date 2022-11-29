ce776c925c8df4314eb607369b3528678e57a9c5
hjc3613 <37894838+hjc3613@users.noreply.github.com>
2022-11-29 09:32:11 +0800

    Update preprocess_train_ds.py

--- preprocess_train_ds.py
+++ preprocess_train_ds.py
@@ -25,34 +25,18 @@ def clean_text(text):
 
     return strQ2B(special2n(text)).lower()
 
-
-def get_dot_index(text, max_split_len):
-    flag = 0
-    text_ = text
-    dot_index_list = []
-
-    while (len(text_) > max_split_len):
-        text_ = text_[:max_split_len]
-        index_list = []
-        for match in re.finditer(r"[,，;；。]", text_):
-            index_list.append(match.span()[0])
-
-        index_list.sort()
-        if len(index_list) > 0:
-            last_dot = index_list.pop()
-        else:
-            last_dot = len(text_)
-        dot_index_list.append(last_dot + flag)
-        text_ = text[(last_dot + flag):]
-        flag += last_dot
-    # 全句都没有标点符号，或者最后一个标点符号不在句末，把结尾处索引也放进来
-    if not dot_index_list or dot_index_list[-1] < len(text):
-        dot_index_list.append(len(text))
-    return dot_index_list
-
-def assign_label(y, start, end, label):
-    assert end - start > 0, "索引不合理！！！"
-    assert y[start:end] == ['O']*(end-start), '标签冲突！！！'
+def assign_label(y, start, end, label, right=1, ent=None, text=None):
+    '''
+    right=1,实体索引右闭，否则右开
+    '''
+    # 子片段是通过**左闭右开**的方式获取的，但是当right==1时，实体是通过**左闭右闭**的方式获取的，因此当ent正好处于句尾时，不对其进行右扩；当其位于句中时，需要右扩一位变为左闭右开
+    # 若right=1，实体右边界为闭区间，应改为开区间
+    end = end + right
+    assert end - start > 0, f"索引不合理！！！start={start}, end={end}"
+    assert y[start:end] == ['O']*(end-start), f'标签冲突！！！y[{start}:{end}]={y[start:end]}, start={start}, end={end}, len(y)={len(y)}'
+    if ent and text:
+        if text[start:end].lower().replace('(', '（').replace(')', '）').replace('，', ',') != ent.lower().replace('(', '（').replace(')', '）').replace('，', ',') :
+            print(f'{text[start:end]:10}\t{ent:10}')
     if end - start > 1:
         labels = ['B-'+label] + ['I-'+label]*(end-start-2) + ['E-'+label]
     else:
@@ -60,7 +44,10 @@ def assign_label(y, start, end, label):
     y[start:end] = labels
     return y
 
-def get_short_text_label(text, shorter_spans, entities, key_start='start', key_end='end', key_label='label'):
+def get_short_text_label(text, shorter_spans, entities, key_start='start', key_end='end', key_label='label', right=1):
+    '''
+    right = 1 代表实体边界为右闭区间，若实体为左闭右开，需将right=0
+    '''
     X = []
     y = []
     for span_s, span_e in shorter_spans:
@@ -71,17 +58,19 @@ def get_short_text_label(text, shorter_spans, entities, key_start='start', key_e
             start = entity[key_start]
             end = entity[key_end]
             label = entity[key_label]
-            if start < span_s or end > span_e:
+            ent = entity.get('ent')
+            # 根据right是否为1，确定实体右边界是否为闭区间，如果是闭区间，需要往右扩1，通过end+right实现
+            if start < span_s or end + right > span_e:
                 continue
-            # 实体在当前短句中的位置
+            # 实体在当前片段中的位置
             offset_s = start - span_s
             offset_e = end - span_s
-            y_ = assign_label(y_, offset_s, offset_e, label)
+            y_ = assign_label(y_, offset_s, offset_e, label, ent=ent, text=text_, right=1)
         X.append(X_)
         y.append(y_)
     return X, y
 
-def get_short_spans(long_txt, max_split_len, level=0, offset=0, debug=False):
+def get_short_spans(long_txt, max_split_len, level=0, offset=0):
     punc_lst = [r'(;|；|。|:|：|$)', r'(,|，|$)', r'(、|$)']
     
     punc = punc_lst[level]
@@ -93,17 +82,21 @@ def get_short_spans(long_txt, max_split_len, level=0, offset=0, debug=False):
         cur_start, cur_end = span.span()
         # 当前span长度不超过max_split_len，尽量往上一个片段拼接，无法拼接时，作为新的span
         if cur_end - cur_start < max_split_len:
-            if len(result) > 0 and cur_end - result[-1][0] < max_split_len:
+            if len(result) > 0 and cur_end - result[-1][0] < max_split_len and level>0:
                 result[-1] = (result[-1][0], cur_end)
             else:
                 result.append((cur_start, cur_end))
         else:
             # 如果当前span长度依然很长，继续深度切分
-            if level > len(punc_lst)-1:
-                sub_spans = [(i, i+max_split_len) for i in range(cur_start, cur_end, max_split_len)]
+            if level + 1 > len(punc_lst)-1:
+                sub_spans = [(i, min(i+max_split_len, len(long_txt))) for i in range(cur_start, cur_end, max_split_len)]
                 result.extend(sub_spans)
             else:
                 sub_spans = get_short_spans(long_txt[cur_start:cur_end], max_split_len, level=level+1, offset=cur_start)
+                sub_text = [long_txt[s:e] for s,e in sub_spans]
+                assert ''.join(sub_text) == long_txt[cur_start:cur_end], "切分有未知bug"
+                if result:
+                    assert sub_spans[0][0] == result[-1][1]
                 result.extend(sub_spans)
     
     def check_result(span):
@@ -114,7 +107,7 @@ def get_short_spans(long_txt, max_split_len, level=0, offset=0, debug=False):
 
 
     result = [(s+offset, e+offset) for s,e in result]
-    if debug and level==0:
+    if level==0:
         sub_text = [long_txt[s:e] for s,e in result]
         assert ''.join(sub_text) == long_txt, "切分有未知bug"
     return result
@@ -135,39 +128,44 @@ def split_too_long_sent(text: str, entities: list, max_split_len=256, key_start=
             start_pos = entity[key_start]
             end_pos = entity[key_end]
             label = entity[key_label]
-            assign_label(y_, start_pos, end_pos, label)
+            ent = entity.get('ent')
+            assign_label(y_, start_pos, end_pos, label, right=1, ent=ent, text=text)
         X.append(X_)
         y.append(y_)
 
     else:
-        shorter_spans = get_short_spans(text, max_split_len, debug=True)
-        X_list, y_list, entity_data_ = get_short_text_label(text, shorter_spans, entities, key_start=key_start, key_end=key_end, key_label=key_label)
+        shorter_spans = get_short_spans(text, max_split_len)
+        X_list, y_list = get_short_text_label(text, shorter_spans, entities, key_start=key_start, key_end=key_end, key_label=key_label)
         X.extend(X_list)
         y.extend(y_list)
-        entity_data.extend(entity_data_)
-
+    return X, y
 
 def preprocess_train_dataset(path, text_col='text', entities_col='entities', key_start='start', key_end='end', key_label='label'):
     if path.endswith('.jsonl'):
+        output_path = path.replace('.jsonl', '_fixed.jsonl')
         with open(path, encoding='utf8') as f:
             data = [json.loads(i.strip()) for i in f.readlines()]
     elif path.endswith('.json'):
+        output_path = path.replace('.json', '_fixed.jsonl')
         with open(path, encoding='utf8') as f:
             data = json.load(f)
     else:
         raise Exception('unrecognized file format: 请提供json或jsonl格式的文件')
 
+    result = []
     for idx, example in tqdm(enumerate(data), total=len(data), desc='截断长句子'):
-        if idx == 108:
-            print('debug')
+        if idx==7816:
+            print()
         assert text_col in example and entities_col in example, '请提供正确的文本列名和实体列名'
         text = example[text_col]
         entities = example[entities_col]
-        split_too_long_sent(text, entities, max_split_len=128,key_start=key_start,
+        X_list, y_list = split_too_long_sent(text, entities, max_split_len=128,key_start=key_start,
                             key_end=key_end, key_label=key_label)
-
+        result.extend([{'text':x, 'labels':y, 'idx':f'{idx}_{offset}', 'orig_text':text} for offset, (x,y) in enumerate(zip(X_list, y_list))])
+    with open(output_path, mode='w') as f:
+        f.write('\n'.join([json.dumps(i, ensure_ascii=False) for i in result]))
 
 if __name__ == '__main__':
     # split_too_long_sent('')
-    preprocess_train_dataset('hjc_检验检查关系抽取-第二批-136_135_20221027172213.jsonl',
-                             entities_col='markedList', text_col='text', key_start='startOffset', key_end='endOffset', key_label='tagName')
+    preprocess_train_dataset('/nlp/hujunchao/medbert-operation/data_disease/source/generated_train_dataset/val_dataset.json',
+                             entities_col='result', text_col='text', key_start='startOffset', key_end='endOffset', key_label='tagName')
